272644c9ff62a107f4405df454d9385397b1b81f
Tyler Jones <tjjones93@gmail.com>
2021-07-28 15:49:35 -0400

    add buefy slider to pixelartoptions

--- src/components/PixelArtOptions.vue
+++ src/components/PixelArtOptions.vue
@@ -1,8 +1,20 @@
 <template>
-    
+    <div id="pixelartoptions">
+        <section>
+            <b-field label="pixel size">
+                <b-slider
+                    :value="25"
+                    :max="50"
+                ></b-slider>
+            </b-field>
+        </section>
+    </div>
 </template>
 
 <script>
+export default {
+    name: 'PixelArtOptions'
+}
 </script>
 
 <style>
