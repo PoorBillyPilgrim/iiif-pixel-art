2404be89a683d27cacb871bcf69c787c4c9159fe
Tyler Jones <tjjones93@gmail.com>
2021-07-29 13:56:44 -0400

    create ready, finished, move, getCursorPos methods for slider

--- src/components/PixelArtSlider.vue
+++ src/components/PixelArtSlider.vue
@@ -1,11 +1,49 @@
 <template>
-    <div class="img-comp-slider">
-    </div>    
+    <div 
+        class="img-comp-slider"
+        @mousedown="ready"
+        @mouseup="finished"
+        @touchstart="ready"
+        @touchend="finished"
+        @mousemove="move"
+    ></div>    
 </template>
 
 <script>
 export default {
-    name: 'PixelArtSlider'
+    name: 'PixelArtSlider',
+    data() {
+        return {
+            height: null,
+            width: null//,
+            //clicked: false
+        }
+    },
+    methods: {
+        ready(event) {
+            event.preventDefault();
+            this.clicked = true;
+            console.log(event);
+        },
+        finished(event) {
+            console.log(event)
+        },
+        move(event) {
+            if (!this.clicked) return;
+            console.log(this.getCursorPos(event))
+        },
+        getCursorPos(event) {
+            let a, x = 0;
+            event = event || window.event;
+            /*get the x positions of the image:*/
+            a = document.getElementById("pixelitimg").getBoundingClientRect();
+            /*calculate the cursor's x coordinate, relative to the image:*/
+            x = event.pageX - a.left;
+            /*consider any page scrolling:*/
+            x = x - window.pageXOffset;
+            return x;
+        }
+    }
 }
 </script>
 
