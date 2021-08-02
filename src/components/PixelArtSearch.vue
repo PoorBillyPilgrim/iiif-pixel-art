690bb57826a86d8c9a9c57c9e7dc88aac025f6de
Tyler Jones <tjjones93@gmail.com>
2021-08-02 16:42:38 -0400

    init search

---
+++ src/components/PixelArtSearch.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <p class="has-text-left mb-1">The Art Institute of Chicago Collection</p>
+        <b-field>
+            <b-autocomplete
+                placeholder="Search by keyword, artist, or reference"
+            ></b-autocomplete>
+        </b-field>
+    </section>    
+</template>
+
+<script>
+    export default {
+        name: 'PixelArtSearch'
+    }
+</script>
+
+<style>
+.input:focus {
+    border-color: #000 !important;
+    box-shadow: 0 0 0 0.125em rgba(0, 0, 0, 0.25) !important;
+}
+</style>
