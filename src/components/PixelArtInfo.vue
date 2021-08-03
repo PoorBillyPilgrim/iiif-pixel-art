76f5b6b54a227e321eb1015e35e44cfe2f94bd80
Tyler Jones <tjjones93@gmail.com>
2021-08-03 17:47:16 -0400

    init PixelArtInfo and populate with artist name, artwork title, place of origin, and link to art institute website

---
+++ src/components/PixelArtInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="box">
+        <h3 class="title is-4 is-spaced">{{imageInfo.title}}</h3>
+        <p class="subtitle is-6">{{imageInfo.artist}}, {{imageInfo.origin}}</p>
+        <a :href="url">View at the Art Institute's website</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PixelArtInfo',
+    props: {
+        imageInfo: Object,
+        api_id: Number
+    },
+    computed: {
+        url: function () {
+            return this.$aic_website + this.api_id;
+        }
+    }
+}
+</script>
+
+<style></style>
