3b3470dae2ff950a998588babd63046acdea93bd
Tyler Jones <tjjones93@gmail.com>
2021-08-03 13:36:04 -0400

    init gallery with successful API call to chicago search returning first 5 public domain images

---
+++ src/components/Gallery.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="gallery">
+        <h2 class="title">Gallery</h2>
+        <div class="gallery-images">
+            <b-button @click="getGalleryImages">get images</b-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Gallery',
+    data() {
+        return {
+            page: 1
+        }
+    },
+    methods: {
+        getGalleryImages() {
+            fetch(this.$api_url + "artworks/search?query[term][is_public_domain]=true&limit=5&page=" + this.page)
+                .then(res => res.json())
+                .then(data => console.log(data.data))
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
