77a4737b69075d38086e49ed69106a5a87035ac4
Tyler Jones <tjjones93@gmail.com>
2021-08-03 10:30:53 -0400

    add NavBar and primary color

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="app">
-      <PixelArt/>
+    <NavBar class="mb-3"/>
+    <PixelArt/>
   </div>
 </template>
 <script>
+import NavBar from '@/components/NavBar.vue'
 import PixelArt from '@/components/PixelArt.vue'
 
 export default {
   name: 'App',
   components: {
+    NavBar,
     PixelArt
   }
 }
 </script>
 
 <style>
+:root {
+  --primary: #2c3e50
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--primary);
 }
 </style>
