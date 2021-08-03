d68f2b5634b40113e694b2f918f2bc0016f6a54b
Tyler Jones <tjjones93@gmail.com>
2021-08-03 11:17:21 -0400

    init Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="hero is-small">
+        <div class="hero-body">
+            <p class="title">
+                PixelArt Institute of Chicago
+            </p>
+            <p class="subtitle">
+                Seeing the collection through pixels
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style>
+
+</style>
