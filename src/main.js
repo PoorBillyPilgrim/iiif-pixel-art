6bfc031964910e3905f874a069e689c0d42ae45f
Tyler Jones <tjjones93@gmail.com>
2021-08-05 15:05:05 -0400

    change image_thumbnail size for iiif images from 200 -> 400

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ Vue.prototype.$aic_website = "https://www.artic.edu/artworks/"
 Vue.prototype.$api_url = "https://api.artic.edu/api/v1/"
 Vue.prototype.$iiif_url = "https://www.artic.edu/iiif/2/"
 Vue.prototype.$image_full_size = "/full/843,/0/default.jpg"
-Vue.prototype.$image_thumbnail = "/full/200,/0/default.jpg"
+Vue.prototype.$image_thumbnail = "/full/400,/0/default.jpg"
 
 new Vue({
   render: h => h(App),
