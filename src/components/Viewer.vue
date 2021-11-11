2bf3a201f6febee61cdd2c8062a4e2c29f22179e
Tyler Jones <tjones357@gatech.edu>
2021-11-11 13:12:58 -0500

    refactor

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -18,40 +18,6 @@ export default {
     },
     mounted: function() {
         this.initViewer()
-        /*this.viewer.addHandler('animation-finish', function(event) {
-            // event.eventSource references the viewer
-            console.log(event)
-        })*/
-        //this.viewer.addHandler('tile-loaded', function(event) {
-          //  console.log(event)
-        //})
-        this.viewer.addHandler('open', (viewer) => {
-            //this.viewer.world.getItemAt(0).lastDrawn.forEach(x => console.log(x))
-            //console.log(viewer)
-            let tiledImg = this.viewer.world.getItemAt(0)
-            tiledImg.addHandler('fully-loaded-change', function() {
-                ///console.log(viewer.eventSource.drawer.canvas.toDataURL())
-                let tile = tiledImg.lastDrawn[0]
-                //viewer.eventSource.addOverlay(tiledImg.lastDrawn[0])
-                let canvas = document.createElement("canvas")
-                //div.style.height = tile.bounds.height
-                //div.style.width = tile.bounds.width
-                /*let overlay = new OpenSeadragon.Overlay({
-                    element: div,
-                    location: tile.bounds,
-                    className: 'overlay'
-                })*/
-                canvas.id = 'test'
-                canvas.className = 'overlay'
-                canvas.style.backgroundColor = 'blue'
-                viewer.eventSource.addOverlay({
-                    element: canvas,
-                    location: tile.bounds,
-                })
-            })
-        })
-
-        
     },
     watch: {
         infoJson: {
@@ -62,17 +28,6 @@ export default {
             },
             deep: true
         }
-        /*viewer: {
-            handler() {
-                /*this.viewer.addHandler('tile-drawn', function() {
-                    this.img = this.viewer.drawer.canvas.toDataURL('image/png')
-                    console.log(this.img)
-                })
-                //console.log(this.viewer.world)
-                console.log('test')
-                //this.onFinish()
-            }
-        }*/
     },
     methods: {
         initViewer() {
@@ -81,20 +36,26 @@ export default {
                 prefixUrl: '//openseadragon.github.io/openseadragon/images/',
                 crossOriginPolicy: 'Anonymous'
             })
-            //this.getImg()
+            this.addOverlay()
         },
-        onFinish() {
-            this.viewer.addHandler('animation-finish', function() {
-                console.log(this.viewer.viewport.getBounds())
+        addOverlay() {
+            this.viewer.addHandler('open', (viewer) => {
+            let tiledImg = this.viewer.world.getItemAt(0)
+                tiledImg.addHandler('fully-loaded-change', function() {
+                    // test adding overlay by grabbing first tile available from lastDrawn array
+                    let tile = tiledImg.lastDrawn[0]
+                    let canvas = document.createElement("canvas")
+                    // test adding tags to overlay
+                    canvas.id = 'test'
+                    canvas.className = 'overlay'
+                    canvas.style.backgroundColor = 'blue'
+                    viewer.eventSource.addOverlay({
+                        element: canvas,
+                        location: tile.bounds,
+                    })
+                })
             })
         }
-        /*,
-        getImg() {
-            this.viewer.addHandler('open', function() {
-                this.img = this.viewer.drawer.canvas.toDataURL('image/png')
-                console.log(this.img)
-            })
-        }*/
     }
 }
 </script>
