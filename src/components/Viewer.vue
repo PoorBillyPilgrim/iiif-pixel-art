88071bb7c6f4ddf1e24f80f150b115245b755157
Tyler Jones <tjones357@gatech.edu>
2021-11-12 10:24:48 -0500

    pixelate TiledImage
    finally was able to successfully pixelate TiledImage in Viewer by just adding img#pixelitimg to template and hiding it from view. Then in addOverlay(), add canvas#pixelitcanvas as an overlay to Viewer set the location as the bounds of TiledImage. Then call pixelate()

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="container">
         <div id="openseadragon"></div>
+        <img id="pixelitimg" :src="iiif_url"/>
         <b-button @click="pixelate">pixelate</b-button>
     </div>
 </template>
@@ -18,12 +19,9 @@ export default {
     },
     data() {
         return {
-            id: null,
             viewer: null,
             tiledImage: null,
-            //overlays: [],
-            img: new Image(),
-            canvas: document.createElement('canvas'),
+            pixelitCanvas: document.createElement('canvas'),
             tiles: [],
             pixelImage: null,
             isPixelated: false,
@@ -36,8 +34,6 @@ export default {
     },
     mounted: function() {
         this.initViewer()
-        //this.getImage()
-        //console.log(this.iiif_url)
     },
     watch: {
         infoJson: {
@@ -47,14 +43,6 @@ export default {
                 })
             },
             deep: true
-        },
-        iiif_url: {
-            handler() {
-                this.img.src = this.iiif_url
-                this.img.id = 'pixelitimg'
-                console.log(this.img)
-            },
-            deep: true
         }
     },
     methods: {
@@ -62,55 +50,37 @@ export default {
             this.viewer = OpenSeadragon({
                 id: 'openseadragon',
                 prefixUrl: '//openseadragon.github.io/openseadragon/images/',
-                crossOriginPolicy: 'Anonymous',
-                debugMode: true
+                crossOriginPolicy: 'Anonymous'
             })
             this.addOverlay()
             
         },
-        getImage() {
-            //this.img.src = this.iiif_url
-            console.log(this.iiif_url)
-        },
         addOverlay() {
-            this.viewer.addHandler('open', async () => {
-                //this.img.src = 
-                //this.img = this.iiif_url
-                //let json = await response.json()
-                console.log('hello')
-                
-                //console.log(this.api_id)
-                /*this.tiledImage = this.viewer.world.getItemAt(0)
-                let imgData
-                this.tiledImage.addHandler('fully-loaded-change', () => {
-                    imgData = viewer.eventSource.drawer.canvas.toDataURL('image/png')
-                    this.img.src = imgData;
-                })
-                
-                this.img.id = 'pixelitimg'
-                this.img.className = 'overlay'
-                this.img.style.backgroundColor = 'blue'
-                this.img.style.opacity = '0.5'
+            this.viewer.addHandler('open', async (viewer) => {
+                this.tiledImage = this.viewer.world.getItemAt(0)
+
+                this.pixelitCanvas.id = 'pixelitcanvas'
+
                 viewer.eventSource.addOverlay({
-                    element: this.img,
-                    location: viewer.eventSource.viewport.getBounds()
-                })
-                this.canvas.id = 'pixelitcanvas'
-                viewer.eventSource.addOverlay({
-                    element: this.canvas,
-                    location: viewer.eventSource.viewport.getBounds()
-                })*/
+                    element: this.pixelitCanvas,
+                    location: this.tiledImage.getBounds()
+                })          
+                this.pixelate()
             })
         },
         pixelate() {
             this.pixelImage = new pixelit(this.pixelArtOptions);
-            this.pixelImage.draw().pixelate().resizeImage().hideFromImg();
+            this.pixelImage.draw().pixelate().hideFromImg();
             this.isPixelated = true;
         }
     }
 }
 </script>
 <style scoped>
+    #pixelitimg {
+        visibility: hidden;
+    }
+
     #openseadragon {
         height: 500px;
         width: 100%;
