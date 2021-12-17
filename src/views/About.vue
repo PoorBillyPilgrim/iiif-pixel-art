3ff03af91461d2b2cbe660ad950fbdb9ffc98517
Tyler Jones <tjjones93@gmail.com>
2021-12-17 12:56:44 -0500

    begin About copy

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="about">
-        <p>This is a test, dawg</p>
+        <h1>About</h1>
+        <p class="block">The <a href="https://iiif.io">International Interoperability Image Framework (IIIF)</a> is a growing standard in cultural heritage organizations for the delivery of high-quality digital content. In providing a consistent and efficient method for sharing images online, IIIF provides organizations an exciting way to share their digital collections. A benefit of IIIF is its flexibiility. IIIF lends itself to more creative kinds of uses, on which this site capitalizes.</p>
+        <p class="block">The images provided are made available through the Art Institute of Chicago's public API which uses IIIF to deliver its images.</p>
     </div>
 </template>
 <script>
