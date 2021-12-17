9b8e48b62ce7c5fefd556d7a6a11f9aa1401b618
Tyler Jones <tjjones93@gmail.com>
2021-12-17 12:24:35 -0500

    add publicPath for deployment to gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    pages: {
-        index: {
-            entry: 'src/main.js',
-            title: 'Pixel Art Institute of Chicago'
-        }
-    }
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/iiif-pixel-art/'
+    : '/'
 }
