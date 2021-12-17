b5c9e1beb374339274616fe4deb4539ace3fc948
Tyler Jones <tjjones93@gmail.com>
2021-12-17 16:10:49 -0500

    add more copy

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,13 @@
 <template>
-    <div id="about">
+    <div id="about" class="content">
         <h1>About</h1>
-        <p class="block">The <a href="https://iiif.io">International Interoperability Image Framework (IIIF)</a> is a growing standard in cultural heritage organizations for the delivery of high-quality digital content. In providing a consistent and efficient method for sharing images online, IIIF provides organizations an exciting way to share their digital collections. A benefit of IIIF is its flexibiility. IIIF lends itself to more creative kinds of uses, on which this site capitalizes.</p>
-        <p class="block">The images provided are made available through the Art Institute of Chicago's public API which uses IIIF to deliver its images.</p>
+        <p class="block">
+            The <a href="https://iiif.io" target="_blank">International Image Interoperability Framework (IIIF)</a> is a growing standard in cultural heritage organizations for delivering high-quality digital content. 
+            In providing a consistent and efficient method for sharing images online, IIIF provides organizations a robust method for sharing their digital collections. 
+            A benefit of IIIF is its flexibiility which lends itself to more creative kinds of uses. 
+            With the help of image viewers like <a href="https://openseadragon.github.io/" target="_blank">OpenSeadragon</a>, which this site utilizes, user can encounter unique viewing experiences.
+        </p>
+        <p class="block">The images provided are all in the public domain and made available through the <a href="http://api.artic.edu/docs/" target="_blank">Art Institute of Chicago's public API</a> which uses IIIF to deliver its images.</p>
     </div>
 </template>
 <script>
