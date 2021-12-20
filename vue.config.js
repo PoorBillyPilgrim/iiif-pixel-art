8df21c25ee3616cf464301837dd9bc7aad3ceaa1
Tyler Jones <tjjones93@gmail.com>
2021-12-20 09:38:07 -0500

    add pages property so title would render properly

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'PixelArt Institute of Chicago'
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/iiif-pixel-art/'
     : '/'
